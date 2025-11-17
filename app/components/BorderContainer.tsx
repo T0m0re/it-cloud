082e2ccd728703c21ed17b1c8b6380aa1e5d32ea
Tomore <hassanwahab147@gmail.com>
2025-11-17 13:54:19 +0100

    Mobile NAv

--- app/components/BorderContainer.tsx
+++ app/components/BorderContainer.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 interface BorderContainerProps {
   children: React.ReactNode;
+  className?: string
 }
 
-const BorderContainer = ({ children }: BorderContainerProps) => {
+const BorderContainer = ({ children, className }: BorderContainerProps) => {
   return (
-    <main className='mx-auto max-w-9/10 border-r border-l border-white'>
+    <main className={`mx-auto max-w-9/10 border-r border-l border-white ${className}`}>
         {children}
     </main>
   )
--- app/components/MobileNavbar.tsx
+++ app/components/MobileNavbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import BorderContainer from "./BorderContainer"
 import { Button } from "./ui/button"
 import { cn } from "~/lib/utils"
+import { Link } from "react-router"
 
 const MobileNavbar = () => {
     const [menuOpen, setMenuOpen] = useState(false)
@@ -34,16 +35,41 @@ export default MobileNavbar
 
 const NavItems = () => {
     return(
-        <nav className="w-full h-dvh  left-0 right-0 bg-black scroll-none z-10">
-            <BorderContainer>
-                <div className="flex items-center justify-between">
-                    <div className="w-[60px]">
-                        <img src="/Logo.svg" alt="logo" className="w-full"/>
+        <nav className="w-full h-dvh left-0 right-0 bg-black scroll-none z-10">
+            <BorderContainer className="h-full">
+                <div className="flex flex-col items-start gap-6 pt-4 px-1">
+                    <div className="flex items-center justify-between w-full">
+                        <div className="w-[70px]">
+                            <img src="/Logo.svg" alt="logo" className="w-full"/>
+                        </div>
+
+                        <div className="w-7 flex flex-col items-center gap-4">
+                            <div className="w-full h-0.5 bg-white rounded-lg group-hover:bg-white/70 rotate-45 translate-y-3"/>
+                            <div className="w-full h-0.5 bg-white rounded-lg group-hover:bg-white/70 -rotate-45 -translate-y-2"/>
+                        </div>
                     </div>
 
-                    <div className="w-7 flex flex-col items-center gap-4">
-                        <div className="w-full h-0.5 bg-white rounded-lg group-hover:bg-white/70 rotate-45"/>
-                        <div className="w-full h-0.5 bg-white rounded-lg group-hover:bg-white/70 -rotate-45"/>
+                    <div className="w-full">
+                        <ul>
+                            <li>Services</li>
+                            <li>About Us</li>
+                            <li>Case Study</li>
+                            <li>Blog</li>
+                        </ul>
+                    </div>
+
+                    <div className="flex items-center gap-3 justify-between">
+                        <Button className="text-black font-semibold bg-white cursor-pointer hover:bg-white/80 rounded-xs transition-all px-6">
+                            Sign In
+                        </Button>
+                        <div className="flex ">
+                            <Link to="/">
+                                 <img src="/linkedIn_icon.png" alt="linkedIn_icon" className="w-7 opacity-70 hover:opacity-100"/>
+                            </Link>
+                            <Link to="/">
+                                <img src="/x_icon.png" alt="X_icon" className="w-7 opacity-70 hover:opacity-100"/>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </BorderContainer>
